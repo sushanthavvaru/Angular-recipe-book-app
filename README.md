4a6c2b7526a77deb18f5b759824364c85a4cb717
sushanthavvaru <sushantha.a@gmail.com>
2017-11-16 18:56:34 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyFirstApp
+# N5CompleteGuide
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.0.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('my-first-app App', () => {
+describe('n5-complete-guide App', () => {
   let page: AppPage;
 
   beforeEach(() => {
