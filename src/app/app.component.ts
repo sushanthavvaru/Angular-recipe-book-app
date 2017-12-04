8a16157c046bf378fb33a543f2dfef736619cf8d
sushanthavvaru <sushantha.a@gmail.com>
2017-12-03 19:59:30 -0800

    Firebase Auth and Database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCJXKlZrvUPESA_uaYlEzJG-oUQ26A7wMA",
+      authDomain: "recepie-book-de8b0.firebaseapp.com",
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
